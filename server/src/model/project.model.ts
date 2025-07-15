535af159ab237839e0fe8175287f0c276ef72790
Ranranruo <tlsalstjr58@gmail.com>
2025-07-15 15:11:56 +0900

    commit

--- server/src/model/project.model.ts
+++ server/src/model/project.model.ts
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 
 const ProjectSchema = new mongoose.Schema({
-    isMobile: { type: Boolean, required: true }
+    title: { type: String },
+    description: { type: String },
+    award: { type: String },
+    role: { type: String, required: true },
+    titleImage: { type: String },
+    isMobile: { type: Boolean, required: true },
+    skills: { type: [String] },
+    images: { type: [String] },
 });
---
+++ server/src/util/fileUploader.ts
@@ -0,0 +1,7 @@
+const fileUploader = {
+    fileUpload: async (file: File, path: String) => {
+        
+    }
+}
+
+export default fileUploader;

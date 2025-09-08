d4bf862091fc7c94d6bba96c4e0fb8f29673bf1c
minseok shin <tlsalstjr58@gmail.com>
2025-09-08 14:33:29 +0900

    feat: connect db

--- server/src/controller/project.controller.ts
+++ server/src/controller/project.controller.ts
@@ -1,11 +1,15 @@
 import { put } from "@vercel/blob";
 import { Request, Response } from "express"
+import { Project } from "../model/project.model";
 
 interface Files {
     titleImage?: Express.Multer.File[];
 }
 
 export const createProject = async (request: Request, response: Response) => {
+    console.log(request.body);
     const files = request.files as Files;
-    
+    const project = new Project({name:"a"});
+    await project.save();
+    response.send("a");
 }
